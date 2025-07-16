7cc098a1b1d543a24827c9e648ab13a549321215
jpmgoncalves12 <jpmgoncalves12@gmail.com>
2025-07-16 11:02:33 -0300

    Fixing Production Docker and health check

--- backend/src/app/routes.ts
+++ backend/src/app/routes.ts
@@ -1,4 +1,4 @@
-import { Router } from 'express';
+import { Router, Request, Response } from 'express';
 
 import groupRoutes from './domains/group/http/routes/group.routes';
 import queueRoutes from './domains/queue/http/routes/queue.routes';
@@ -6,6 +6,12 @@ import sessionRoutes from './domains/user/http/routes/session.routes';
 import userRoutes from './domains/user/http/routes/user.routes';
 
 const router = Router();
+
+router.get('/health', async (_: Request, response: Response) => {
+  response.send({
+    message: 'OK',
+  });
+});
 
 router.use('/group', groupRoutes);
 router.use('/queue', queueRoutes);
